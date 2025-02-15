252c1a1d97f407c2f3e0de7a98ba11190ebcf9dc
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-02-15 23:09:00 +0530

    Update SelctionSort.py

--- Sorting/SelctionSort.py
+++ Sorting/SelctionSort.py
@@ -15,3 +15,8 @@ def Sorted_List(List_1):
 List_1 = [23, 43, 12, 3, 45, 6, 5]
 print("Original List:", List_1)
 print("Sorted List:", Sorted_List(List_1))
+
+# Case	         Time Complexity	Explanation
+# Best Case	      O(n²)	             Still O(n²), because comparisons happen in every case.
+# Average Case	  O(n²)	             Consistently quadratic due to nested loops.
+# Worst Case	  O(n²)	             Occurs when elements are in reverse order.
