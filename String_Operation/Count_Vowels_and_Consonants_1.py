819c3f40ffc78d811ac1b5b1a8e37e3846a21647
Akshith KL <160204272+akshithkl@users.noreply.github.com>
2025-04-10 15:58:07 +0530

    Add files via upload

---
+++ String_Operation/Count_Vowels_and_Consonants_1.py
@@ -0,0 +1,8 @@
+def count_vowels_consonants(s):
+    vowels = "aeiouAEIOU"
+    v_count = sum(1 for char in s if char in vowels)
+    c_count = sum(1 for char in s if char.isalpha() and char not in vowels)
+    return v_count, c_count
+
+# Example
+print(count_vowels_consonants("Hello World"))  # Output: (3, 7)
